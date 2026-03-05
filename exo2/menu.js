3e7b947916809cc0bcf4bbbe7b864c61921d4142
Faraz109 <mohammadfaraz.cinna@my-digital-school.org>
2026-03-05 11:01:27 +0100

    menu exo2

---
+++ exo2/menu.js
@@ -0,0 +1,9 @@
+let nav = document.querySelector("nav");
+
+nav.innerHTML = `
+<a href="index.html">Index</a> |
+<a href="elements.html">Elements</a> |
+<a href="zoo.html">Zoo</a> |
+<a href="feutres.html">Feutres</a> |
+<a href="planetes.html">Planetes</a>
+`;
